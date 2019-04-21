4b94ac956036af6ce00c5a52f4e079f27743b3b2
Kimberly Hermosillo <kimhermosillo@yahoo.com>
2019-04-21 15:44:35 -0500

    needs styling

---
+++ src/components/Filter.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+export default class Products extends Component {
+
+    render() {
+
+
+        return (
+            <div className="row">
+                <div className="col-md-4">
+                    {`${this.props.count} products found.`}
+                </div>
+                <div className="col-md-4">
+                    <label>Order by
+               <select className="form-control" value={this.props.sort} onChange={this.props.handleSortChange}>
+                            <option value="">Select</option>
+                            <option value="lowestprice">Lowest to highest</option>
+                            <option value="highestprice">Highest to lowest</option>
+                        </select>
+                    </label>
+                </div>
+            </div>
+        )
+    }
+}
